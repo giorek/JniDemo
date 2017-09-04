fb7be7fe0fdedd056f46468ed44bc6cd80be4a0f
徐鹏 <xupeng@qiyi.com>
2017-09-04 18:12:40 +0800

    1,增加jni层调用android日志的宏定义。2，增加java层调用jni层传递基础类型，传递数组类型，以及数组类型在jni层的一些操作，在jni层调用Java端方法与获取变量的方法

--- AndroidJniDemo/app/src/main/cpp/native-lib.cpp
+++ AndroidJniDemo/app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,18 @@
 #include <jni.h>
 #include <string>
 
+#include <android/log.h>
+
 /**
  * {@link http://blog.csdn.net/jiangwei0910410003/article/details/17465457}
  * http://blog.csdn.net/jiangwei0910410003/article/details/17466369
+ * http://blog.csdn.net/virus026/article/details/25213157
+ *
+ * jni数组相关处理参考资料
+ * http://blog.csdn.net/jiangwei0910410003/article/details/17653803
+ *
  * *env java环境变量，可以用于对java端进行操作，例如创建java端对象，方法调用，获取属性
- * jobject      若是static 则掉膘当前native方法的类的class对象实例
+ * jobject      若是static 则代表当前native方法的类的class对象实例
  * jobject      若当前方法非static 则代表当前方法的类实例
  * jclass       jni中标识java端class
  * jfieldID     jni中标识java端属性
@@ -37,7 +44,28 @@
  *      Method      (参数1签名,.....)返回值类型签名
  *
  *      实例可以参考下面定义
+ *
+ *
+ * 常用命令行
+ *  javah -jni clazz    根据java文件生成头文件
  */
+
+#define TAG "JNI_DEMO"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG,__VA_ARGS__);
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__);
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG,__VA_ARGS__);
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG,__VA_ARGS__);
+#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE,TAG,__VA_ARGS__);
+#define LOGU(...) __android_log_print(ANDROID_LOG_UNKNOWN,TAG,__VA_ARGS__);
+
+/**
+ * 日志打印
+ * __android_log_print(ANDROID_LOG_INFO,TAG,"hahahahahahahaha");
+ * 调用jni.h中的方法，参数1：日志级别。参数2：相当于android日志打印中的tag值。参数3：需要打印出的信息
+ * 也可以使用define进行替代
+ * 前提是需要引入<android/log.h>头文件
+ */
+
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_qiyi_androidjnidemo_MainActivity_stringFromJNI(
@@ -47,12 +75,95 @@ Java_com_qiyi_androidjnidemo_MainActivity_stringFromJNI(
 
     //获取类名，属性名和方法名
     jclass java_claz = env->GetObjectClass(obj);
-    jfieldID fieldId_property = env->GetFieldID(java_claz,"property","I");
-    jmethodID methodId_function = env->GetMethodID(java_claz,"","(ILjava/util/Date;[I)I");
+    jfieldID fieldId_property = env->GetFieldID(java_claz, "property", "I");
+    jmethodID methodId_function = env->GetMethodID(java_claz, "function", "(ILjava/util/Date;[I)I");
+    env->CallIntMethod(obj, methodId_function, 1, NULL, NULL);
 
+    //调用java端参数为char的函数，返回值为void
+    jmethodID methodId_arg_char = env->GetMethodID(java_claz, "function", "(C)V");
+    const char c = 'K';
+    env->CallVoidMethod(obj, methodId_arg_char, c);
 
+    //调用java端参数为String的函数
+    jmethodID methodId_arg_str = env->GetMethodID(java_claz, "functionString",
+                                                  "(Ljava/lang/String;)V");
+    jstring str = env->NewStringUTF("中华人民共和国万岁");
+    env->CallVoidMethod(obj, methodId_arg_str, str);
 
-
-    jstring jStr = env->NewStringUTF("Hello C++");
     return env->NewStringUTF(hello.c_str());
+}
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_qiyi_androidjnidemo_MainActivity_callJniGetJavaInrAry(JNIEnv *env, jobject instance) {
+
+    jclass claz = env->GetObjectClass(instance);
+    jfieldID fileID_java_int_Array = env->GetFieldID(claz, "globalIntAry", "[I");
+    jintArray obj_java_int_array = (jintArray) env->GetObjectField(instance, fileID_java_int_Array);
+    jint array_len = env->GetArrayLength(obj_java_int_array);
+    jint *int_array = env->GetIntArrayElements(obj_java_int_array, nullptr);
+    for (int i = 0; i < array_len; i++) {
+        LOGI("obj_java_int_array[%d]=%d", i, int_array[i]);
+    }
+
+    //创建本地数组，并将java端数组传递到这里
+    jintArray local_int_ary = env->NewIntArray(array_len);
+    jint *loacal_int = env->GetIntArrayElements(local_int_ary, nullptr);
+    for (int j = 0; j < array_len; j++) {
+        loacal_int[j] = int_array[j];
+        LOGI("local_int_ary[%d]=%d", j, loacal_int[j]);
+    }
+
+    env->ReleaseIntArrayElements(obj_java_int_array, int_array, 0);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_qiyi_androidjnidemo_MainActivity_callJniArgIntAry(JNIEnv *env, jobject instance,
+                                                           jintArray array_) {
+    /**
+     *从java端传递的数据转换到c/c++中，参数2表示在做传递时是否进行拷贝，true，将上层数据做一份拷贝传递给本地代码。false:不做拷贝直接将java层数组的指针传递给本地代码
+     * 需要和Release配对使用
+     */
+    jint *array = env->GetIntArrayElements(array_, NULL);
+    //获取java层传递的数组长度
+    jint ary_length = env->GetArrayLength(array_);
+    LOGI("callJniArgIntAry length of array_:%d", ary_length);
+    for (int i = 0; i < ary_length; i++) {
+        LOGI("callJniArgIntAry array_:[%d]:%d", i, array[i]);
+    }
+
+    /**
+     * 释放时参数一是java端数组，参数二是本地c/c++端数据，参数三是内存释放模式
+     * 0：更新java端数据，并释放本地数组
+     * JNI_COMMIT：对Java端数组进行更新但是不释放本地数组内存
+     * JNI_ABORT：对java端数组不进行更新但是释放本地数组内存
+     */
+    env->ReleaseIntArrayElements(array_, array, 0);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_qiyi_androidjnidemo_MainActivity_callJniArgChar(JNIEnv *env, jobject instance, jchar c) {
+
+    LOGI("callJniArgChar c:%c", c);
+
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_qiyi_androidjnidemo_MainActivity_callJniArgInt(JNIEnv *env, jobject instance, jint i) {
+
+    LOGI("callJniArgInt i:%d", i);
+
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_qiyi_androidjnidemo_MainActivity_callJniArgStr(JNIEnv *env, jobject instance,
+                                                        jstring str_) {
+    const char *str = env->GetStringUTFChars(str_, 0);
+    LOGI("JniDemo run here %s:", str);
+    env->ReleaseStringUTFChars(str_, str);
 }
--- AndroidJniDemo/app/src/main/java/com/qiyi/androidjnidemo/MainActivity.java
+++ AndroidJniDemo/app/src/main/java/com/qiyi/androidjnidemo/MainActivity.java
@@ -2,16 +2,20 @@ package com.qiyi.androidjnidemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "JNI_DEMO";
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+
+    public int[] globalIntAry = {21,22,23};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +25,58 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        //传递string到底层
+        callJniArgStr("Hello 中国！");
+        //传递int
+        callJniArgInt(100);
+
+        callJniArgChar('p');
+
+        int[] iArray = {10,11,12};
+        callJniArgIntAry(iArray);
+
+        callJniGetJavaInrAry();
     }
 
     public int property;
     public int function(int i, Date date,int[] ary){
+        Log.d(TAG," java client call function");
+        if(null != ary){
+            for(int j = 0; j < ary.length; j++ ){
+                Log.d(TAG," java client call function ary["+j+"]:"+ary[j]);
+            }
+        }
+        Log.d(TAG," java client call function end");
         return 1;
+    }
+
+    public void function(char c){
+        Log.d(TAG," java client call function c :"+c);
+    }
+
+    public void functionString(String str){
+        Log.d(TAG," java client call functionString str :"+str);
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
+     * 非空调用
      */
     public native String stringFromJNI();
+
+    //传递string到jni层
+    public native void callJniArgStr(String str);
+
+    //传递int到jni层
+    public native void callJniArgInt(int i);
+
+    //传递char到jni层
+    public native void callJniArgChar(char c);
+
+    //传递int[]到jni层
+    public native void callJniArgIntAry(int[] array);
+
+    public native void callJniGetJavaInrAry();
 }
